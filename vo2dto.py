f83ce26fab0a9682617d6595aa9df8d967f11fce
xulujun <xulujun@kuaishou.com>
2023-09-26 17:25:42 +0800

    vo2dto

--- vo2dto.py
+++ vo2dto.py
@@ -15,14 +15,14 @@ PATH_SEP = '/'
 home_dir = os.environ["HOME"]
 
 vo_map = {}
-vo_map.setdefault("BaseVO", "BaseDTO")
+vo_map.setdefault("BaseVO", dict(vo='BaseVO', dto="BaseDTO"))
 test_vo_file = ''
 test_update_file = ''
 
 ## 排除的文件
 excludes = ['*/.git/*', '*/.idea/*', '*/target/*', '*.DS_Store', "*.iml", "*/generate.py", "*/archetype/*"]
 # includes = ["*/pom.xml", "*/CheckApiService.java"]
-includes = []
+includes = ['*.java']
 read_me_md = r'*/readme.md'
 
 
@@ -52,23 +52,32 @@ def accept(path):
 def process_line(line):
     if not len(line):
         return line
-
     new_line = line
-    for key, val in vo_map.items():
-        new_line = new_line.replace(key, val)
-
+    for val in vo_map.values():
+        new_line = new_line.replace(val['vo'], val['dto'])
     return new_line
 
 
-def write_file(target_root, source_root, source_full_path, lines):
-    target_full_path = source_full_path.replace(source_root, target_root)
-    print(target_full_path)
-    target_dir = os.path.dirname(target_full_path)
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
+def is_vo_file(path):
+    return path.endswith("VO.java") or path.endswith("Vo.java")
+
+
+def write_file(lines, file_name, path):
+    target_dir = os.path.dirname(path)
+    is_vo = False
+    target_full_path = path
+    new_file_name = ''
+    if is_vo_file(path):
+        vo_meta = vo_map.get(path)
+        is_vo = True
+        new_file_name = os.path.join(target_dir, vo_meta['dto'] + '.java', )
+    print('write file:%s' % target_full_path)
     f = open(target_full_path, r'w+')
     [f.write(line) for line in lines]
     f.close()
+    if is_vo:
+        print("rename %s to %s" % (path, new_file_name))
+        os.rename(path, new_file_name)
 
 
 def register_vo(source_dir):
@@ -76,9 +85,9 @@ def register_vo(source_dir):
     for root, dirs, files in list_dirs:
         for f in files:
             path = os.path.join(root, f)
-            if accept(path) and (f.endswith("VO.java") or f.endswith("Vo.java")):
-                # val = dict(path=path, dto=re.sub('(VO|Vo).java', 'DTO', f))
-                vo_map.setdefault(f.replace('.java', ''), re.sub('(VO|Vo).java', 'DTO', f))
+            if accept(path) and is_vo_file(path):
+                val = dict(vo=f.replace('.java', ''), path=path, dto=re.sub('(VO|Vo).java', 'DTO', f))
+                vo_map.setdefault(path, val)
                 print("register VO:%s" % (path))
 
 
@@ -89,10 +98,10 @@ def do_vo2dto(source_dir):
     for root, dirs, files in list_dirs:
         for f in files:
             path = os.path.join(root, f)
-            if accept(path) and (test_update_file == f):
+            if accept(path) and (test_update_file == '' or test_update_file == f):
                 print("try update %s" % (f))
                 new_lines = [process_line(line) for line in read_file(path)]
-                print(''.join(new_lines))
+                write_file(new_lines, f, path)
 
 
 def vo_to_do(project_path, test_vo_file, test_update_file):
@@ -101,6 +110,7 @@ def vo_to_do(project_path, test_vo_file, test_update_file):
         raise IOError("project dir not exists, " + source_dir)
     register_vo(source_dir)
     do_vo2dto(source_dir)
+
 
 def main():
     if len(sys.argv) < 2 or '-h' in sys.argv or "--help" in sys.argv:
